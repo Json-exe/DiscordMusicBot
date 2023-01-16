4770a9543b2baf746c87d30bd4b43eee14f8dd0e
Jason <lucaheitfeld@gmail.com>
2023-01-16 20:33:04 +0100

    Removed support for remote files, like radios, bc they dont work on linux hosting

--- DiscordMusicBotV2/Commands/PlayCommand.js
+++ DiscordMusicBotV2/Commands/PlayCommand.js
@@ -8,124 +8,6 @@ const queue = require("../index.js").queue;
 const wait = require('node:timers/promises').setTimeout;
 const {playlist_info, search, video_info, spotify, refreshToken, is_expired} = require('play-dl');
 
-async function playRemoteMP3(interaction, songURL, serverQueue) {
-    // Get all songs from the playlist if the URL is a playlist
-    let songInfo;
-    const failedSongs = [];
-    try {
-        let resource;
-        if (songURL.endsWith(".mp3")) {
-            resource = createAudioResource(songURL, {
-                inputType: StreamType.Arbitrary,
-                inlineVolume: true
-            });
-        } else if (songURL.endsWith(".ogg")) {
-            resource = createAudioResource(songURL, {
-                inputType: StreamType.OggOpus,
-                inlineVolume: true
-            });
-        } else if (songURL.endsWith(".webm")) {
-            resource = createAudioResource(songURL, {
-                inputType: StreamType.WebmOpus,
-                inlineVolume: true
-            });
-        } else {
-            resource = await createAudioResource(songURL, {
-                inputType: StreamType.Arbitrary,
-                inlineVolume: true,
-            });
-        }
-
-        console.log("Creating player");
-
-        // Create a player
-        const player = createAudioPlayer();
-        player.on('error', error => {
-            console.error(error);
-            return interaction.editReply("Failed to play your stream!");
-        }).on('stateChange', (oldState, newState) => {
-            console.log(`Player transitioned from ${oldState.status} to ${newState.status}`);
-            if (oldState.status === AudioPlayerStatus.Buffering && newState.status === AudioPlayerStatus.Playing) {
-                console.log("Changed from buffering to playing");
-            }
-        }).on('autopaused', () => {
-            wait(1000);
-            player.unpause();
-        }).on('buffering' , () => {
-            console.log("Buffering...");
-        });
-        await player.play(resource);
-        const test = player.checkPlayable();
-        console.log(test);
-        console.log(resource.ended);
-        console.log(resource.read())
-        await wait(500);
-        if (!test || resource.ended) {
-            player.stop(true);
-            return interaction.editReply("Failed to play your stream!");
-        }
-        player.stop(true);
-    } catch (error) {
-        await console.log(error);
-        await wait(2000);
-        return await interaction.editReply("Failed to create Stream, try checking the URL!");
-    }
-    // Remove http/https from the URL if it exists
-    const song = {
-        title: songURL,
-        url: songURL,
-        duration: null,
-        requestedBy: interaction,
-        thumbnail: "https://images.unsplash.com/photo-1614680376573-df3480f0c6ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=280&q=100"
-    };
-    // Create the added to queue embed
-    const addedToQueueEmbed = new EmbedBuilder()
-        .setTitle('ADDED TO QUEUE')
-        .setColor(0x0000ff)
-        .setDescription(`:white_check_mark: \`${song.title}\``)
-        .setThumbnail(song.thumbnail)
-        .addFields({
-            name: "ㅤ",
-            value: `Added by ${interaction.member} | Position: \`❯ ${serverQueue?.songs?.length > 0 ? serverQueue.songs.length : 1}\``,
-        });
-    if (!serverQueue || !serverQueue.connection || serverQueue.connection.state.status === VoiceConnectionStatus.Destroyed || serverQueue.connection.state.status === VoiceConnectionStatus.Disconnected ||
-        serverQueue.songs.length === 0) {
-        // Check if connection is destroyed
-        const queueContruct = {
-            textChannel: interaction.channel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true,
-            audioPlayer: null,
-            loop: false
-        };
-        await queue.set(interaction.guild.id, queueContruct);
-        queueContruct.songs.push(song);
-        try {
-            const channel = await interaction.member.voice.channel;
-            var connection = joinVoiceChannel({
-                channelId: channel.id, // 994907171982692361
-                guildId: channel.guild.id, // 994907168484642928
-                adapterCreator: await channel.guild.voiceAdapterCreator,
-                selfDeaf: true,
-                selfMute: false
-            });
-            queueContruct.connection = connection;
-            await console.log("Joined Voice Channel " + channel.name);
-            await main.play(interaction.guild, queueContruct.songs[0]);
-            return await interaction.editReply({embeds: [addedToQueueEmbed]});
-        } catch (err) {
-            await console.log(err);
-            queue.delete(interaction.guild.id);
-            return await interaction.editReply("Error Occurred: " + err);
-        }
-    } else {
-        serverQueue.songs.push(song);
-        return await interaction.editReply({embeds: [addedToQueueEmbed]});
-    }
-}
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
@@ -160,9 +42,8 @@ module.exports = {
         } else if (songURL.includes("youtube")) {
             await youtubeLinks(interaction, songURL, serverQueue);
         } else {
-            await playRemoteMP3(interaction, songURL, serverQueue);
-            /*await wait(1000);
-            return await interaction.editReply({ embeds: [ new EmbedBuilder().setTitle("Invalid URL Type").setColor(0x0000ff) ] });*/
+            await wait(1000);
+            return await interaction.editReply({ embeds: [ new EmbedBuilder().setTitle("Invalid URL Type. Bot only supports YT and Spotify Links!").setColor(0x0000ff) ] });
         }
     }
 }
